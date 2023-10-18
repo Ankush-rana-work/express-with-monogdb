4d91a27dc3f764fb4c95d95dcb326a1bf97a2cd8
Ankush Rana <75374330+Ankush-rana-work@users.noreply.github.com>
2023-10-18 12:35:01 +0530

    Update db.js
    removed mongo url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://ankushrana:9560736256Ank@cluster0.ht7cwmi.mongodb.net/nodeWithMongo?retryWrites=true&w=majority';
+const MONGODB_URI = '';
 const MONGODB_DB_NAME = 'nodeWithMongo';
 
 // Replace 'your-mongodb-connection-string' with your actual MongoDB connection string
